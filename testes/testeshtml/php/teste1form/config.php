c3eaf10bfcafefda83869568f4234910e36b02f4
Joao3243 <109562443+Joao3243@users.noreply.github.com>
2022-11-24 10:11:55 -0300

    Add files via upload

---
+++ testes/testeshtml/php/teste1form/config.php
@@ -0,0 +1,15 @@
+<?php
+    $dbHost='Localhost';
+    $dbUsername ='root';
+    $dbPassword='';
+    $dbName = 'form-jh';
+    $conexao=new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+    //if($conexao->connect_errno)
+    //{
+    //   echo 'erro';
+    //}
+    //else{
+    //    echo 'conexão efetuada com sucesso';
+    //}
+    
+?>
---
+++ testes/testeshtml/php/teste1form/index.js
@@ -0,0 +1,9 @@
+function direcione(event){
+    if(event.which===13){
+        let nome=document.querySelector('input').value
+        let email=document.querySelectorAll('input')[1].value
+        let password=document.querySelectorAll('input')[2].value
+        
+    }
+        
+}
